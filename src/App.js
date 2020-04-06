d011245df73685d069837e9efbcf90c529717f56
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-04-06 17:59:11 -0300

    chore: configurate components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import mha from './assets/01-02.png';
-
 function App() {
   return <img width='1000' src={mha} />;
 }
---
+++ src/components/TechList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+
+class Techlist extends Component {
+  render() {
+    
+  }
+}
